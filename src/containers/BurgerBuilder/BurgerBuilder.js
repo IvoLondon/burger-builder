0c73696b3ddaa3f3e065b714d418a828988c791a
Ivo <ivelin.iliev@dusted.com>
2018-07-13 18:07:38 +0100

    BuildControl, Burger and Burger Ingredients

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/BuildControls/BuildControls';
+
+
+
+class BurgerBuilder extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			ingredients : {
+				salad : 0,
+				bacon : 0,
+				cheese : 0,
+				meat : 0,
+			}
+		}
+	}
+	render() {
+		return (
+			<div>
+				<Burger ingredients={this.state.ingredients} />
+				<BuildControls />
+			</div>
+		)
+	}
+}
+
+export default BurgerBuilder
